af30f9c11e7d5f2126228a29ad28bb0ca86c77f2
jeremyj <ccj6161@163.com>
2026-02-04 08:20:03 +0000

    修改生成的条目数量

--- scripts/generate_instructions_agent.sh
+++ scripts/generate_instructions_agent.sh
@@ -2,6 +2,6 @@ batch_dir=data_agent/gpt3_generations/
 
 python self_instruct/bootstrap_instructions_agent.py \
     --batch_dir ${batch_dir} \
-    --num_instructions_to_generate 20 \
+    --num_instructions_to_generate 5000 \
     --seed_tasks_path seed/seed_tasks_openscenario_agent.jsonl \
     --engine "davinci"
