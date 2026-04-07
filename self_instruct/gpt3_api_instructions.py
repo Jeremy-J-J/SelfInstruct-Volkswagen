a450c16a751615d57bff43512f47c21f4f999406
jeremyj <ccj6161@163.com>
2026-04-07 02:00:00 +0000

    update

--- self_instruct/gpt3_api_instructions.py
+++ self_instruct/gpt3_api_instructions.py
@@ -63,16 +63,16 @@ def make_requests(
     # if organization is not None:
     #     openai.organization = organization
     
-    # client = OpenAI(api_key=api_key, base_url="http://localhost:8007/v1")
+    client = OpenAI(api_key=api_key, base_url="http://localhost:8007/v1")
     # client = OpenAI(api_key=api_key, base_url="http://10.160.199.227:8028/v1") # qwen3-coder
-    client = OpenAI(api_key=api_key, base_url="http://10.160.199.103:30738/v1") # qwen3-coder-next
+    # client = OpenAI(api_key=api_key, base_url="http://10.160.199.103:30738/v1") # qwen3-coder-next
     retry_cnt = 0
     backoff_time = 30
     while retry_cnt <= retries:
         try:
             for p in prompts:
                 resp = client.chat.completions.create(
-                    model="qwen3-coder-next", 
+                    model="holo-model", 
                     messages=[{"role": "system", "content": SYSTEM_PROMPT}, {"role": "user", "content": p}],
                     max_tokens=target_length,
                     temperature=temperature,
